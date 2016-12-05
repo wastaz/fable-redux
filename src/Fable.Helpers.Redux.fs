08a6f1cd34f6f7e7e2b3549db40b5fd4c5f842d9
Fredrik Forssen <fredrik.forssen@tretton37.com>
2016-12-05 20:21:14 +0100

    Update the stuff to fable 0.7

--- src/Fable.Helpers.Redux.fs
+++ src/Fable.Helpers.Redux.fs
@@ -1,11 +1,10 @@
-﻿[<Fable.Core.Erase>]
-module internal Fable.Helpers.Redux
+﻿module Fable.Helpers.Redux
 
 open Fable.Core.JsInterop
 
 open Fable.Import
 
-let inline createStore (reducer : 'TState -> 'TAction -> 'TState) (initialState : 'TState) (middleware : Redux.Middleware option) =
+let createStore (reducer : 'TState -> 'TAction -> 'TState) (initialState : 'TState) (middleware : Redux.Middleware option) =
     let rdc = fun state action ->
         match box action?Case with
         | :? string -> (System.Func<_,_,_> reducer).Invoke(state, action)
@@ -14,8 +13,11 @@ let inline createStore (reducer : 'TState -> 'TAction -> 'TState) (initialState 
     | Some(m) -> Redux.createStore(System.Func<_,_,_> rdc, initialState, m)
     | None -> Redux.createStore(System.Func<_,_,_> rdc, initialState)
 
-let inline unionMiddleware (_ : Redux.IUntypedStore) (next : obj -> unit) (action : obj) =
-    if action.GetType().Name <> "Function" then
+[<Fable.Core.Emit("$0 instanceof Function")>]
+let isFunction a : bool = failwith "JS Only"
+
+let unionMiddleware (_ : Redux.IUntypedStore) (next : obj -> unit) (action : obj) =
+    if isFunction action then
         let o = toPlainJsObj action
         if o?hasOwnProperty$("Case") |> unbox<bool> then 
             let o = toPlainJsObj action
