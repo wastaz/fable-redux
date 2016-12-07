b85de828a05faa50464f3226ea8191d65ff6d64d
Fredrik Forssen <fredde@grodslok.net>
2016-12-07 21:54:14 +0100

    Fix fable core versin and union middleware

--- src/Fable.Helpers.Redux.fs
+++ src/Fable.Helpers.Redux.fs
@@ -5,19 +5,15 @@ open Fable.Core.JsInterop
 open Fable.Import
 
 let createStore (reducer : 'TState -> 'TAction -> 'TState) (initialState : 'TState) (middleware : Redux.Middleware option) =
-    let rdc = fun state action ->
-        match box action?Case with
-        | :? string -> (System.Func<_,_,_> reducer).Invoke(state, action)
-        | _ -> state
     match middleware with
-    | Some(m) -> Redux.createStore(System.Func<_,_,_> rdc, initialState, m)
-    | None -> Redux.createStore(System.Func<_,_,_> rdc, initialState)
+    | Some(m) -> Redux.createStore(System.Func<_,_,_> reducer, initialState, m)
+    | None -> Redux.createStore(System.Func<_,_,_> reducer, initialState)
 
 [<Fable.Core.Emit("$0 instanceof Function")>]
 let isFunction a : bool = failwith "JS Only"
 
 let unionMiddleware (_ : Redux.IUntypedStore) (next : obj -> unit) (action : obj) =
-    if isFunction action then
+    if not <| isFunction action then
         let o = toPlainJsObj action
         if o?hasOwnProperty$("Case") |> unbox<bool> then 
             let o = toPlainJsObj action
