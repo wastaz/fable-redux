b5774fe6d396eb185bd3a6593b88e863afd92542
Fredrik Forssen <fredde@grodslok.net>
2016-12-07 22:28:38 +0100

    Special case for handling redux init event

--- src/Fable.Helpers.Redux.fs
+++ src/Fable.Helpers.Redux.fs
@@ -5,6 +5,10 @@ open Fable.Core.JsInterop
 open Fable.Import
 
 let createStore (reducer : 'TState -> 'TAction -> 'TState) (initialState : 'TState) (middleware : Redux.Middleware option) =
+    let rdc = fun state action ->
+        match box action?``type`` with
+        | :? string as s when s = "@@redux/INIT" -> state
+        | _ -> (System.Func<_,_,_> reducer).Invoke(state, action)
     match middleware with
-    | Some(m) -> Redux.createStore(System.Func<_,_,_> reducer, initialState, m)
-    | None -> Redux.createStore(System.Func<_,_,_> reducer, initialState)
+    | Some(m) -> Redux.createStore(System.Func<_,_,_> rdc, initialState, m)
+    | None -> Redux.createStore(System.Func<_,_,_> rdc, initialState)
